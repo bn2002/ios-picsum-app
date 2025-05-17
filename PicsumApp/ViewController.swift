d089463f535ccc4edc326f46a0d7f3d6c75a6c97
BN2002 <doanhcms@gmail.com>
2025-05-17 10:11:02 +0700

    Xóa Storyboard, SceneDelegate

--- PicsumApp/ViewController.swift
+++ PicsumApp/ViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    private let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "Hello, World!"
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
 
 
