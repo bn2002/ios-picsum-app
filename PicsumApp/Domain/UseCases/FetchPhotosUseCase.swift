3205b64c89fb9723d5e75483997ccacfcae84e3a
BN2002 <doanhcms@gmail.com>
2025-05-18 10:10:38 +0700

    Thêm Fetch và Search UseCase

--- PicsumApp/Domain/UseCases/FetchPhotosUseCase.swift
+++ PicsumApp/Domain/UseCases/FetchPhotosUseCase.swift
@@ -5,3 +5,25 @@
 //  Created by Nguyễn Duy Doanh on 17/5/25.
 //
 
+import Foundation
+
+protocol FetchPhotosUseCaseProtocol {
+    @discardableResult
+    func execute(page: Int, limit: Int, completion: @escaping (Result<[Photo], Error>) -> Void) -> URLSessionDataTask?
+}
+
+final class FetchPhotosUseCase: FetchPhotosUseCaseProtocol {
+    private let repository: PhotoRepositoryProtocol
+    
+    init(repository: PhotoRepositoryProtocol) {
+        self.repository = repository
+    }
+    
+    @discardableResult
+    func execute(page: Int, limit: Int, completion: @escaping (Result<[Photo], any Error>) -> Void) -> URLSessionDataTask? {
+        return repository.fetchPhotos(page: page, limit: limit, completion: completion)
+    }
+
+}
+
+
--- PicsumApp/Domain/UseCases/SearchPhotosUseCase.swift
+++ PicsumApp/Domain/UseCases/SearchPhotosUseCase.swift
@@ -4,4 +4,28 @@
 //
 //  Created by Nguyễn Duy Doanh on 17/5/25.
 //
+import Foundation
 
+protocol SearchPhotosUseCaseProtocol {
+    func execute(query: String, in photos: [Photo]) -> [Photo]
+}
+
+final class SearchPhotosUseCase: SearchPhotosUseCaseProtocol {
+    private let maxLength = 15
+    
+    private lazy var allowedCharset: CharacterSet = {
+        var allowedCharset = CharacterSet.alphanumerics
+        allowedCharset.formUnion(CharacterSet(charactersIn: "!@#$%^&*():.,<>/\\[]?"))
+        
+        return allowedCharset
+    }()
+    
+    func execute(query: String, in photos: [Photo]) -> [Photo] {
+        let lowercasedQuery = query.lowercased()
+        return photos.filter { photo in
+            return  photo.author.lowercased().contains(lowercasedQuery) ||
+                    photo.id.lowercased().contains(lowercasedQuery)
+            
+        }
+    }
+}
