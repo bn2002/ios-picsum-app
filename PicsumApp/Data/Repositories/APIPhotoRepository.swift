27e2e7e8838c31a9da7a2fe24a97ad227edf5fc3
BN2002 <doanhcms@gmail.com>
2025-05-18 10:09:44 +0700

    Hoàn thiện Photo Repository

--- PicsumApp/Data/Repositories/APIPhotoRepository.swift
+++ PicsumApp/Data/Repositories/APIPhotoRepository.swift
@@ -4,4 +4,23 @@
 //
 //  Created by Nguyễn Duy Doanh on 17/5/25.
 //
+import Foundation
 
+class APIPhotoRepository: PhotoRepositoryProtocol {
+    private let network: NetworkService
+    private let baseURL = "https://picsum.photos/v2/list"
+    
+    init(network: NetworkService) {
+        self.network = network
+    }
+    
+    func fetchPhotos(page: Int, limit: Int, completion: @escaping (Result<[Photo], any Error>) -> Void) -> URLSessionDataTask? {
+        let urlString = "\(baseURL)?page=\(page)&limit=\(limit)"
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NetworkError.invalidURL))
+            return nil
+        }
+        
+        return self.network.request(url: url, completion: completion)
+    }
+}
--- PicsumApp/Data/Repositories/PhotoRepositoryProtocol.swift
+++ PicsumApp/Data/Repositories/PhotoRepositoryProtocol.swift
@@ -4,4 +4,8 @@
 //
 //  Created by Nguyễn Duy Doanh on 17/5/25.
 //
+import Foundation
 
+protocol PhotoRepositoryProtocol {
+    func fetchPhotos(page: Int, limit: Int, completion: @escaping (Result<[Photo], Error>) -> Void) -> URLSessionDataTask?
+}
