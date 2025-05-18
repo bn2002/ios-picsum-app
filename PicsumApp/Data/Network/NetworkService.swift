a190ebc3344e6f953347e58c0000b3cb6586b6a0
BN2002 <doanhcms@gmail.com>
2025-05-18 10:08:50 +0700

    Hoàn thiện NetworkService

--- PicsumApp/Data/Network/NetworkService.swift
+++ PicsumApp/Data/Network/NetworkService.swift
@@ -4,4 +4,44 @@
 //
 //  Created by Nguyễn Duy Doanh on 17/5/25.
 //
+import Foundation
 
+enum NetworkError: Error {
+    case noData
+    case invalidURL
+}
+
+final class NetworkService {
+    private let session: URLSession
+    
+    init() {
+        let config = URLSessionConfiguration.default
+        self.session = URLSession(configuration: config)
+    }
+    
+    @discardableResult
+    func request<T: Decodable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) -> URLSessionDataTask {
+        let task = session.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
+                return
+            }
+            
+            do {
+                let result = try JSONDecoder().decode(T.self, from: data)
+                completion(.success(result))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+        return task
+    }
+
+}
